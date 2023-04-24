48d87ff9292da2107c4c411d87bc372b2ed0ac72
Rosebud <rkle408@gmail.com>
2023-04-24 16:38:12 -0700

    formatted titles

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutUs = () => {
   return (
     <div>
-      About Us
+      <h1 className='title'>About Us</h1>
     </div>
   );
 }
--- src/Pages/FAQ.js
+++ src/Pages/FAQ.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FAQ = () => {
   return (
     <div>
-      FAQ
+      <h1 className='title'>FAQ</h1>
     </div>
   );
 }
