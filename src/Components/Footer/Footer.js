31b0cd3bfee4d96959dacf59a59cf4dce84127f7
Rosebud <rkle408@gmail.com>
2023-04-25 16:30:19 -0700

    moved socials to header

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,10 +5,7 @@ function Footer() {
 
     return (
         <div className="footer">
-            <a href="https://www.yelp.com/biz/best-care-auto-garden-grove" target="_blank" rel="noopener noreferrer"><img src='./yelp.png' alt="Yelp Logo"></img></a> 
-            <a href="https://www.instagram.com/bestcareauto/" target="_blank" rel="noopener noreferrer"><img src='./instagram.png' alt="Instagram Logo"></img></a>
-            <a href="https://www.facebook.com/bestcareauto/" target="_blank" rel="noopener noreferrer"><img src='./facebook.png' alt="Facebook Logo"></img></a>
-            
+
         </div>
     )
 
