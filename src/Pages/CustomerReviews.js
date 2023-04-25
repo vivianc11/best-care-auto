9e145193eac406a7dd3bcba8ae4a5af1692e6d24
Vivian Chen <vivian.chen1193@gmail.com>
2023-04-25 16:37:14 -0700

    adding yelp api to get customer reviews

--- src/Pages/CustomerReviews.js
+++ src/Pages/CustomerReviews.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 const CustomerReviews = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+    const apiKey = process.env.REACT_APP_YELP_API_KEY;
+    const businessId = 'best-care-auto-garden-grove';
+
+    const reviewsUrl = `https://api.yelp.com/v3/businesses/${businessId}/reviews`;
+
+    const headers = {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json'
+    };
+
+    axios.get(reviewsUrl, { headers })
+        .then(response => {
+            console.log(response.data.reviews);
+            // process the reviews data here
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    return (
+        <div>
+
+        </div>
+    );
 }
 
 export default CustomerReviews;
