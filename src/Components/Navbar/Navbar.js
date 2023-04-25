5bd5b1767a7fb54a66b089c159eceb4b0592a651
Vivian Chen <vivian.chen1193@gmail.com>
2023-04-24 18:04:02 -0700

    fixed hamburger menu so it is aligned to the right

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,13 +7,13 @@ import './Navbar.css';
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Container fluid>
+        <Navbar.Toggle className='ms-auto' aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto">
-            <Nav.Link className='px-5' href="/">About Us</Nav.Link>
-            <Nav.Link className='px-5' href="/services">Services</Nav.Link>
-            <Nav.Link className='px-5' href="/faq">FAQs</Nav.Link>
+            <Nav.Link className='px-5 mx-5' href="/">About Us</Nav.Link>
+            <Nav.Link className='px-5 mx-5' href="/services">Services</Nav.Link>
+            <Nav.Link className='px-5 mx-5' href="/faq">FAQs</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
