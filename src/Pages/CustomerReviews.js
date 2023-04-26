ba49b1912dbbb1e47e4e11f6f1152398bebeaae7
Vivian Chen <vivian.chen1193@gmail.com>
2023-04-26 15:19:04 -0700

    created 6 cards for reviews

--- src/Pages/CustomerReviews.js
+++ src/Pages/CustomerReviews.js
@@ -1,40 +1,164 @@
-import React, { useState, useEffect } from 'react';
-import yelp from '../api/yelp';
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import './CustomerReview.css';
+
 
 const CustomerReviews = () => {
-    const [reviews, setReviews] = useState([]);
 
+  return (
+    <div className='review-card-container'>
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <div className='card-top'>
+            <div className='reviewer-info'>
+              <img src='' alt='Pic of Reviewer' />
+              <div className='name-and-location'>
+                <Card.Title>Reviewer Name</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Location</Card.Subtitle>
+              </div>
+            </div>
+            <img src='' alt='rating' />
+          </div>
 
-    useEffect(() => {
-        const fetchReviews = async () => {
-          try {
-            const response = await yelp.get('/best-care-auto-garden-grove/reviews', {
-              params: {
-                limit: 10,
-                sort_by: 'yelp_sort',
-              },
-            });
-            console.log(response.data.reviews)
-            setReviews(response.data.reviews);
-          } catch (err) {
-            console.log(err);
-          }
-        };
-    
-        fetchReviews();
-      }, []);
+          <Card.Text>
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+          </Card.Text>
 
+        </Card.Body>
+        <Card.Footer className="text-muted"><a href=''>Read more on Yelp</a></Card.Footer>
+      </Card>
 
-    return (
-        <div>
-            {reviews.map((review) => (
-                <div key={review.id}>
-                    <h2>{review.user.name}</h2>
-                    <p>{review.text}</p>
-                </div>
-            ))}
-        </div>
-    )
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <div className='card-top'>
+            <div className='reviewer-info'>
+              <img src='' alt='Pic of Reviewer' />
+              <div className='name-and-location'>
+                <Card.Title>Reviewer Name</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Location</Card.Subtitle>
+              </div>
+            </div>
+            <img src='' alt='rating' />
+          </div>
+
+          <Card.Text>
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+          </Card.Text>
+
+        </Card.Body>
+        <Card.Footer className="text-muted"><a href=''>Read more on Yelp</a></Card.Footer>
+      </Card>
+
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <div className='card-top'>
+            <div className='reviewer-info'>
+              <img src='' alt='Pic of Reviewer' />
+              <div className='name-and-location'>
+                <Card.Title>Reviewer Name</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Location</Card.Subtitle>
+              </div>
+            </div>
+            <img src='' alt='rating' />
+          </div>
+
+          <Card.Text>
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+          </Card.Text>
+
+        </Card.Body>
+        <Card.Footer className="text-muted"><a href=''>Read more on Yelp</a></Card.Footer>
+      </Card>
+
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <div className='card-top'>
+            <div className='reviewer-info'>
+              <img src='' alt='Pic of Reviewer' />
+              <div className='name-and-location'>
+                <Card.Title>Reviewer Name</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Location</Card.Subtitle>
+              </div>
+            </div>
+            <img src='' alt='rating' />
+          </div>
+
+          <Card.Text>
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+          </Card.Text>
+
+        </Card.Body>
+        <Card.Footer className="text-muted"><a href=''>Read more on Yelp</a></Card.Footer>
+      </Card>
+
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <div className='card-top'>
+            <div className='reviewer-info'>
+              <img src='' alt='Pic of Reviewer' />
+              <div className='name-and-location'>
+                <Card.Title>Reviewer Name</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Location</Card.Subtitle>
+              </div>
+            </div>
+            <img src='' alt='rating' />
+          </div>
+
+          <Card.Text>
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+          </Card.Text>
+
+        </Card.Body>
+        <Card.Footer className="text-muted"><a href=''>Read more on Yelp</a></Card.Footer>
+      </Card>
+
+      <Card style={{ width: '20rem' }}>
+        <Card.Body>
+          <div className='card-top'>
+            <div className='reviewer-info'>
+              <img src='' alt='Pic of Reviewer' />
+              <div className='name-and-location'>
+                <Card.Title>Reviewer Name</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Location</Card.Subtitle>
+              </div>
+            </div>
+            <img src='' alt='rating' />
+          </div>
+
+          <Card.Text>
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+            Review Review Review Review Review
+          </Card.Text>
+
+        </Card.Body>
+        <Card.Footer className="text-muted"><a href=''>Read more on Yelp</a></Card.Footer>
+      </Card>
+    </div>
+  )
 
 }
 
