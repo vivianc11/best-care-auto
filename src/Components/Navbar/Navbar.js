662e88f4d30045ac95c73c33371d0e5411bcd88d
Vivian Chen <vivian.chen1193@gmail.com>
2023-04-27 11:05:01 -0700

    trying to make hamburger visible

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -17,19 +17,22 @@ const NavBar = () => {
   }
 
   return (
-    <Navbar className='nav-bar' bg="secondary" expand="lg">
-      <Container fluid>
-        <Navbar.Toggle className='ms-auto' aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="m-auto">
-            <Nav.Link className='px-5 mx-5 hover-shadow' style={styles.nav} href="/">About Us</Nav.Link>
-            <Nav.Link className='px-5 mx-5 hover-shadow' style={styles.nav} href="/services">Services</Nav.Link>
-            <Nav.Link className='px-5 mx-5 hover-shadow' style={styles.nav} href="/faq">FAQs</Nav.Link>
-            <Nav.Link className='px-5 mx-5 hover-shadow' style={styles.nav} href="/customerreviews">Customer Reviews</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar className='nav-bar' bg="" expand="lg">
+        <Container fluid>
+          <Navbar.Toggle className='ms-auto' aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="m-auto">
+              <Nav.Link className='px-5 mx-5' href="/">About Us</Nav.Link>
+              <Nav.Link className='px-5 mx-5' href="/services">Services</Nav.Link>
+              <Nav.Link className='px-5 mx-5' href="/faq">FAQs</Nav.Link>
+              <Nav.Link className='px-5 mx-5' href="/customerreviews">Customer Reviews</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+
   );
 }
 
